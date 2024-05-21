4d948b5366d306ffffd8411cca9824a01449f01a
danielbosa <danielbosa95@gmail.com>
2024-05-21 09:22:43 +0200

    Content in home to be fixed (images). Last part of content in home to be added (as design).
    At least another page with same layout (and info from db)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,17 +7,27 @@
 @section('title', 'Home')
 
 @section('content')
-<section class="container">
-    <h1>I nostri prodotti</h1>
-    <ul>
-    @foreach ( $comics as $comic)
-        <li>{{$comic['title']}}</li>
-
-    @endforeach
-    </ul>
-
-
-
-</section>
+<main class="">
+    <div class="db-container pb-4">
+        <span class="db-btn db-btn-1">Current series</span>
+        <div class="container py-4">
+            <div class="row g-3">
+                @foreach ( $comics as $comic)
+                    <div class="col-4 col-md-3 col-lg-2 h-100">
+                        <div class="card">
+                            <div class="db-card-img-container">
+                                <img class="db-card-img" :src="{{$comic['thumb']}}" alt="">
+                            </div>
+                            <div class="db-card-title text-uppercase">{{$comic['title']}}</div>
+                        </div> 
+                    </div>
+                @endforeach
+            </div>
+        </div>
+        <div class="text-center">
+            <button class="db-btn db-btn-2">Load more</button>
+        </div>    
+    </div>
+</main>
 
 @endsection
